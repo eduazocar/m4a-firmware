a0f5031167b91c974c7e0980c2033c58cbafafd0
eduazocar <93989980+eduazocar@users.noreply.github.com>
2022-04-27 05:59:31 +0000

    Deploying to gh-pages from @ eduazocar/m4a-firmware@99e8d2f4491ab511eb673d33a64d56a412f28d49 🚀

--- group__examples.js
+++ group__examples.js
@@ -1,21 +1,9 @@
 var group__examples =
 [
-    [ "Getting started", "index.html#autotoc_md25", [
-      [ "Getting Started:", "md_examples_ds18b20_README.html#autotoc_md27", null ],
-      [ "Go to the example folder", "md_examples_ds18b20_README.html#autotoc_md28", null ],
-      [ "Compile your code and flash it", "md_examples_ds18b20_README.html#autotoc_md29", null ],
-      [ "First", "md_examples_ds18b20_README.html#autotoc_md30", null ],
-      [ "Last steps", "md_examples_ds18b20_README.html#autotoc_md31", null ]
-    ] ],
-    [ "How to test it", "md_examples_external_led_README.html#autotoc_md33", [
-      [ "Go to the example folder", "md_examples_slipdev_README.html#autotoc_md34", null ],
-      [ "Compile your code and flash it", "md_examples_slipdev_README.html#autotoc_md35", null ],
-      [ "First", "md_examples_slipdev_README.html#autotoc_md36", null ],
-      [ "Uart example", "group__tmp__uart.html#autotoc_md37", null ]
-    ] ],
+    [ "Getting started", "index.html#autotoc_md29", null ],
     [ "Analog capacitive moisture sensor", "group__moisture__sensor__example.html", null ],
     [ "Ds18b20", "group__ds18b20.html", null ],
     [ "External-led blink", "group__external-led.html", null ],
     [ "Slipdev", "group__slipdev.html", "group__slipdev" ],
-    [ "Uart", "group__tmp__uart.html", "group__tmp__uart" ]
+    [ "Uart", "group__tmp__uart.html", null ]
 ];
